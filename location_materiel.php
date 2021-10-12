4c6f0fc795a8f3625b1610d6a0e0089ffd8f2d7e
aubin4444 <91742876+aubin4444@users.noreply.github.com>
2021-10-12 18:29:56 +0200

    Add files via upload

--- location_materiel.php
+++ location_materiel.php
@@ -2,7 +2,7 @@
 	session_start();
 
 	$nom=$_SESSION['nom'];
-	
+	$id = query(SEL)
 ?>
 
 <!DOCTYPE html>
@@ -13,66 +13,38 @@
 </head>
 <body>
 	<h1>Location de matériel</h1>
+	<fieldset>
+		<legend>Vos locations en cours</legend>
+			<?php
+				$bdd = new PDO('mysql:host=localhost;dbname=badminton', 'root', 'root');
 
-<form method="post">
-	<input type="submit" name="BT_loc" value="Location en cours">
-	<input type="submit" name="BT_new_loc" value="Démarrer une location">
-</form>
+				$requete = $bdd->prepare("SELECT type_materiel, date_emprunt, date_retour, quantite FROM materiel INNER JOIN adherent ON id_emprunteur = adherent.id WHERE nom = ?");
 
+				$requete->execute(array($nom));
 
-<?php
-	$bdd = new PDO('mysql:host=localhost;dbname=badminton', 'root', '');
+				while ($donnees=$requete->fetch()) 
+				{
+					echo $donnees['quantite'].' '.$donnees['type_materiel'].' du '.$donnees['date_emprunt'].' au '.$donnees['date_retour'].'<br />';
+				}
+			?>	
+	</fieldset>
+	<br>
+	<form method="post" action="formulaire_nouvelle_location.html">
+		<input type="submit" value="Démarrer une location">
+	</form>
 
-	if (isset($_POST['BT_loc']))
- 	{
-	
-		$requete = $bdd->prepare("SELECT type_materiel, date_emprunt, date_retour, quantite FROM materiel INNER JOIN adherent ON id_emprunteur = adherent.id WHERE nom = ?");
-		$requete->execute(array($nom));
-	?> <p>Vous avez loué : <br></p> <?php
-
-		while ($donnees=$requete->fetch()) {
-		echo $donnees['quantite'].' '.$donnees['type_materiel'].' du '.$donnees['date_emprunt'].' au '.$donnees['date_retour'].'<br />';
-		}
-	}
-	if (isset($_POST['BT_new_loc'])){
-		?>
-		<fieldset>
-		<form method="post">
-
-		
-		<label for="type_materiel">Type de matériel<br><br></label>
-
-		<select name="materiel">
-    		<option value="">--Veuillez sélectionner le matériel que vous souhaitez louer--</option>
-    		<option value="raquette">raquette</option>
-    		<option value="vollant">vollant</option>
-    		<option value="tenue">tenue</option>
-		</select>
-		
-		<label for="date_emprunt"><br><br>date de l'emprunt</label>
-		<p><input type="date" name="date_emprunt"></p>
-		
-		<label for="date_retour">date de retour</label>
-		<p><input type="date" name="date_retour"></p>
-		
-		<label for="quantite">Quantité</label>
-		<p><input type="text" name="quantite"></p>
-		
-		<input type="submit" name="Valider" value="Valider">
-		</form>
-		</fieldset>
 	<?php
-	if (isset($_POST['Valider'])){
-	$req=$bdd->prepare('INSERT INTO materiel(type_materiel, date_emprunt, date_retour, quantite) VALUES (?,?,?,?)');
-	$req->execute(array(
-		 $_POST['materiel'],
-		 $_POST['date_emprunt'],
-		 $_POST['date_retour'],
-		 $_POST['quantite'],
-		 
-		 ));
-	}
-	}
+	
+		if (isset($_POST['Valider']))
+		{
+			$req=$bdd->prepare('INSERT INTO materiel(date_emprunt, type_materiel, date_retour, quantite) VALUES (?,?,?,?)');
+			$req->execute(array(
+			$_POST['date_emprunt'],
+			 $_POST['type_materiel'],
+			 $_POST['date_retour'],
+			 $_POST['quantite']));
+		}
+	
 	?>
 	
 	<a href="connection_compte.PHP">Retourner sur votre espace utilisateur</a>
