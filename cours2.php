f3defb06194cc845a46c7143d67bab070f105b08
aubin4444 <91742876+aubin4444@users.noreply.github.com>
2021-10-19 14:22:36 +0200

    Add files via upload

---
+++ cours2.php
@@ -0,0 +1,34 @@
+
+<html>
+<head>
+	<title> </title>
+</head>
+<body>
+<fieldset>
+	<form>
+		<label>Veuillez saisir le jour pour lequel vous souhaitez rejoindre un cours</label>
+		<br><br>
+		<label>Lundi
+		<input type="radio" name="day" value="Lundi">
+		</label>
+		<label>Mardi
+		<input type="radio" name="day" value="Mardi">
+		</label>
+		<label>Mercredi
+		<input type="radio" name="day" value="Mercredi">
+		</label>
+		<label>Jeudi
+		<input type="radio" name="day" value="Jeudi">
+		</label>
+		<label>Vendredi
+		<input type="radio" name="day" value="Vendredi">
+		</label>
+	</form>
+</fieldset>
+
+<?php
+	if(isset('day'))
+?>
+
+</body>
+</html>
