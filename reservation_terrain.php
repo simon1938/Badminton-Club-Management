db474f2368e85dc32906ce6f96fd4a548a6d0c50
simon1938 <84197669+simon1938@users.noreply.github.com>
2021-10-19 16:13:18 +0200

    Add files via upload

--- reservation_terrain.php
+++ reservation_terrain.php
@@ -7,26 +7,32 @@
 <body>
 	<h1>Menus des réservations terrains </h1>
 	<h2>Veuillez compléter tout les champs concernant votre réservation </h2>
-		<fieldset>
-		<from methode="post" action="">
 
-		<label for="numero_terrain">Le numéro de Terrain</label>
-		<p><input type="int" name="numero_terrain"></p>
-		
-		<label for="date_reservation">date de reservation</label>
-		<p><input type="date" name="date_reservation"></p>
-		
-		<label for="horaire">Votre Horaire</label>
-		<p><input type="time" name="horaire"></p>
-		
+
+		<fieldset>
+
+
+		<form method="post" action="reservation_terrain.php">
+
+
+        <label for="semaine_reservation">Semaine de réservation</label>
+        <p><input type="number" name="semaine_reservation"></p>
+
+
+		<label for="jour_reservation">Jour de la reservation</label>
+		<p><input type="text" name="jour_reservation"></p>
+
             <p><label for="type_terrain">Type de Terrain</label></p>
             <select name="type_terrain">
-            <option value="Simple">Simple</option>
-            <option value="Double">Double</option>
-           <p> </select></p>
+                <option value="Simple">Simple</option>
+                <option value="Double">Double</option>
+                <p> </select></p>
+		
+
 
 
             <p><label for="nombre_invite">Selectionner le nombre d'adherents avec qui vous voulez jouer </label></p>
+
             <select name="nombre_invite">
             <option value="1">1</option>
             <option value="2">2</option>
@@ -34,46 +40,67 @@
                 <p></select></p>
 
 
-
-        <?php
-        if(isset($_POST['1'])){
-            echo 'salut la co';
-
-        }
-
-        ?>
-
-
-
-		
 		<label for="autre_adherent">Adherent partageant votre réservation </label>
 		<p><select name="autre_adherent"></p>
 
-		<?php
+            <?php
 
-		$bdd = new PDO('mysql:host=localhost;dbname=badminton;charset=utf8', 'root', '');
+            $bdd = new PDO('mysql:host=localhost;dbname=badminton;charset=utf8', 'root', '');
 
-		$reponse=$bdd->query('SELECT nom FROM adherent');
-        $reponse->execute();
+            $reponse=$bdd->query('SELECT nom FROM adherent');
+            $reponse->execute();
 
-		while ($donnees=$reponse->fetch())
-		 {
-		 	?>
-		 	<option value="<?php echo $donnees['nom'];?>"> <?php echo $donnees['nom'];?> </option>
-		 
-					 
-		 
-	<?php
-			
+            while ($donnees=$reponse->fetch())
+            {
+                ?>
+                <option value="<?php echo $donnees['nom'];?>"> <?php echo $donnees['nom'];?> </option>
+
+
+
+                <?php
 				}
 
 		?>
-		</select>
-		
-		 </from>	
 
-		</fieldset>
+
+		</select>
+
+            <input type="submit" name="Valider">
+
+
 		
-	
+		 </form>
+
+
+    <?php
+    if(isset($_POST['Valider']))
+    {
+        echo '
+        <form method="post" action "resultat_reservation_terrain">
+             <p><label for="resultat">Voici les terrains disponibles en fonction de vos choix </label></p>
+             <select name="resultat">';
+
+          
+         $rep=$bdd->query("SELECT email FROM adherent");
+          $rep->execute();
+
+		while ($donnee=$rep->fetch())
+		 {
+		     ?>
+             <option value="<?php echo $donnee['email'];?>"> <?php echo $donnee['email'];?> </option>
+
+            <?php
+         }
+    }
+            ?>
+
+
+
+
+            </select>  
+            </form>
+
+
+        </fieldset>
 </body>
 </html>
