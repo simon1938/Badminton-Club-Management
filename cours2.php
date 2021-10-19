3559f747e51fc70c8977ec05119afec1630f995f
aubin4444 <91742876+aubin4444@users.noreply.github.com>
2021-10-19 16:13:03 +0200

    Add files via upload

--- cours2.php
+++ cours2.php
@@ -5,29 +5,49 @@
 </head>
 <body>
 <fieldset>
-	<form>
-		<label>Veuillez saisir le jour pour lequel vous souhaitez rejoindre un cours</label>
+	<form method="post">
+		<label>Choisir le cours en fonction de (date/horaire)<input required type="radio" name="choix" value="date"> </label>
 		<br><br>
-		<label>Lundi
-		<input type="radio" name="day" value="Lundi">
-		</label>
-		<label>Mardi
-		<input type="radio" name="day" value="Mardi">
-		</label>
-		<label>Mercredi
-		<input type="radio" name="day" value="Mercredi">
-		</label>
-		<label>Jeudi
-		<input type="radio" name="day" value="Jeudi">
-		</label>
-		<label>Vendredi
-		<input type="radio" name="day" value="Vendredi">
-		</label>
+        <label>Choisir le cours en fonction du prof<input required type="radio" name="choix" value="prof"> </label>
+        <br><br>
+        <input type="submit" name="envoyer">
 	</form>
 </fieldset>
 
 <?php
-	if(isset('day'))
+if(isset($_POST['envoyer'])) {
+
+    if ($_POST['choix'] == "date")
+    {
+        echo 'Vous avez choisis l\'option date';
+        ?>
+        <fieldset>
+            <form method="post">
+                <label>Sélectionner un jour<input type="date" name="jour" max="01/01/2023"> </label>
+                <br><br>
+                <label>Sélectionner une plage horaire
+                    <select>
+                        <option>8:00 - 9:00</option>
+                        <option>9:00 - 10:00</option>
+                        <option>10:00 - 11:00</option>
+                        <option>11:00 - 12:00</option>
+                        <option>12:00 - 13:00</option>
+                        <option>13:00 - 14:00</option>
+                        <option>14:00 - 15:00</option>
+                        <option>15:00 - 16:00</option>
+                        <option>16:00 - 17:00</option>
+                    </select> </label>
+                <br><br>
+                <input type="submit" name="envoyer">
+            </form>
+        </fieldset>
+        <?php
+    }
+    else
+    {
+        echo 'Vous avez choisis l\'option prof';
+    }
+}
 ?>
 
 </body>
