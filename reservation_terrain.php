538a2d9da3b6b5a5a8866d61a792081780301551
simon1938 <84197669+simon1938@users.noreply.github.com>
2021-10-24 19:04:31 +0200

    Add files via upload

--- reservation_terrain.php
+++ reservation_terrain.php
@@ -8,10 +8,8 @@
 	<h1>Menus des réservations terrains </h1>
 	<h2>Veuillez compléter tout les champs concernant votre réservation </h2>
 		<fieldset>
-		<from methode="post" action="">
+		<form methode="POST" action="resultat_reservation_terrain.php">
 
-		<label for="numero_terrain">Le numéro de Terrain</label>
-		<p><input type="int" name="numero_terrain"></p>
 		
 		<label for="date_reservation">date de reservation</label>
 		<p><input type="date" name="date_reservation"></p>
@@ -21,27 +19,19 @@
 		
             <p><label for="type_terrain">Type de Terrain</label></p>
             <select name="type_terrain">
+
             <option value="Simple">Simple</option>
             <option value="Double">Double</option>
            <p> </select></p>
 
 
             <p><label for="nombre_invite">Selectionner le nombre d'adherents avec qui vous voulez jouer </label></p>
-            <select name="nombre_invite">
+            <select name="nombre_invite" id="nombre_invite">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
                 <p></select></p>
 
-
-
-        <?php
-        if(isset($_POST['1'])){
-            echo 'salut la co';
-
-        }
-
-        ?>
 
 
 
@@ -51,7 +41,8 @@
 
 		<?php
 
-        include 'connexion_bdd.php';
+		$bdd = new PDO('mysql:host=localhost;dbname=badminton;charset=utf8', 'root', '');
+
 		$reponse=$bdd->query('SELECT nom FROM adherent');
         $reponse->execute();
 
@@ -68,11 +59,15 @@
 
 		?>
 		</select>
+            <p><input type="submit" name="valider" value="Valider"</p>
 		
-		 </from>	
+		 </form>
 
 		</fieldset>
-		
+
+    <a href="connection_compte.PHP">Retourner sur votre espace utilisateur</a>
 	
 </body>
 </html>
+
+
